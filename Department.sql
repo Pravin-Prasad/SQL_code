660cd94e6fe840a76188e87a2d845708a209a351
Prasad <pravin.prasad@capgemini.com>
2023-01-19 17:19:44 +0530

    files successfully added

---
+++ Department.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Department(
+deptid INTEGER PRIMARY KEY,
+deptname TEXT NOT NULL,
+deptloc TEXT NOT NULL);
+
+INSERT INTO Department VALUES(001,'HR','Pune');
+INSERT INTO Department VALUES(002,'Executive','Hyderabad');
+INSERT INTO Department VALUES(003,'Account','Delhi');
+INSERT INTO Department VALUES(004,'Admin','Chennai');
